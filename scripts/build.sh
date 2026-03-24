34fb484b1f690150f43ce89e47050cd6ad40b076
渣渣120 <WOSHIZHAZHA120@qq.com>
2026-03-24 21:04:22 +0800

    :construction_worker: 移除分离的 `tpsecore` 构建逻辑

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,22 @@ git rev-parse --short HEAD > resources/ci-commit # commit for display purposes
 cat resources/ci-commit-previous
 cat resources/ci-commit
 cat resources/release-commit
+git pull --recurse-submodules # ensure tpsecore is pulled in
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
+rustup default 1.87.0
+rustup target add wasm32-unknown-unknown
+
+# build it
+git submodule init
+git submodule update
+cd tpsecore
+cargo build --quiet --profile release --target wasm32-unknown-unknown
+cp target/wasm32-unknown-unknown/release/tpsecore.wasm ../source/lib
+cp tpsecore.js ../source/lib
+cd ..
 
 # build script dependencies
 DEBIAN_FRONTEND=noninteractive apt-get install zip -y -qq
